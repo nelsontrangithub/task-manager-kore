552d5b2d04e50ea6b30bc16fae0e631696373fa6
nelsontrangithub <nelsontran@icloud.com>
2019-04-18 13:23:26 -0700

    added logic to seeder for Users

--- kore_api/kore_api/Util/DbSeeder.cs
+++ kore_api/kore_api/Util/DbSeeder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kore_api.koredb;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,44 @@ namespace kore_api.Util
 {
     public class DbSeeder
     {
+        public static void Initialize(koredbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            ////Look for any users.
+            //if (context.ApplicationUsers.Any())
+            //    {
+            //        return;   // DB has been seeded
+            //    }
+
+            var users = new User[]
+            {
+                new User
+                {
+                       Id = 1,
+                       Email = "admin@kore.com",
+                       Name = "Joe Smith",
+                       DateCreated = DateTime.Now,
+                       Status = 1,
+                       FirstName = "Joe",
+                       LastName = "Smith"
+                },
+                new User
+                {
+                       Id = 2,
+                       Email = "user@kore.com",
+                       Name = "Anne Smith",
+                       DateCreated = DateTime.Now,
+                       Status = 1,
+                       FirstName = "Joe",
+                       LastName = "Smith"
+                }
+            };
+            foreach (User s in users)
+            {
+                context.User.Add(s);
+            }
+            context.SaveChanges();
+        }
     }
 }
