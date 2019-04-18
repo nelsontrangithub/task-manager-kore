0685a142eea2efa9a4c52dda52c81e0e697fd520
tinaachuaxi <xchu3@my.bcit.ca>
2019-04-18 12:45:09 -0700

    put login page in a seperate file

--- kore_app/lib/utils/routes.dart
+++ kore_app/lib/utils/routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kore_app/screens/contractDetail.dart';
 import 'package:kore_app/screens/contractList.dart';
-import 'package:kore_app/main.dart';
+import 'package:kore_app/screens/login.dart';
 
 final routes = {
   '/contractList':         (BuildContext context) => new ContractList(),
