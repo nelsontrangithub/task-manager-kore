59e43dd94d8efc3ec7010d0e9e53f4f5e8bc3c8a
nelsontrangithub <nelsontran@icloud.com>
2019-04-19 15:32:36 -0700

    added IAccountsRepository, AccountsRepository

--- kore_api/kore_api/Controllers/AccountsController.cs
+++ kore_api/kore_api/Controllers/AccountsController.cs
@@ -3,47 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using kore_api.koredb;
+using kore_api.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace kore_api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountsController : ControllerBase
+    public class AccountsController : Controller
     {
-        private koredbContext context;
+        private readonly koredbContext _context;
 
         public AccountsController(koredbContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
         // GET: api/Accounts
         [HttpGet]
         public IEnumerable<Account> Get()
         {
-            var accounts = context.Account.ToList();
-
+            AccountsRespository accRepo = new AccountsRespository(_context);
+            var accounts = accRepo.GetAccounts();
             return accounts;
         }
 
         [HttpPost("{id}")]
-        public bool UpdateStatus(int id, [FromBody] int status)
+        public bool UpdateAccount(int id, [FromBody] int status)
         {
-            var account = context.Account.Where(a => a.Id == id).FirstOrDefault();
-            account.Status = status;
-            account.DateModified = DateTime.Now;
-            try
-            {
-                context.Update(account);
-                context.SaveChanges();
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            return true;
+            return new AccountsRespository(_context).UpdateAccount(id, status);
         }
+
     }
 }
