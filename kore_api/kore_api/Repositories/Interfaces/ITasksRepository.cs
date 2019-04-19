85572f26c830f50be244ed52df7008bb06002c2d
nelsontrangithub <nelsontran@icloud.com>
2019-04-19 16:23:04 -0700

    added TasksRepository

---
+++ kore_api/kore_api/Repositories/Interfaces/ITasksRepository.cs
@@ -0,0 +1,16 @@
+﻿using kore_api.koredb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task = kore_api.koredb.Task;
+
+namespace kore_api.Repositories.Interfaces
+{
+    public interface ITasksRepository
+    {
+        IEnumerable<Task> GetTasks();
+        Task GetTask();
+        bool UpdateTask();
+    }
+}
---
+++ kore_api/kore_api/Repositories/TasksRepository.cs
@@ -0,0 +1,27 @@
+﻿using kore_api.koredb;
+using kore_api.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kore_api.Repositories
+{
+    public class TasksRepository : ITasksRepository
+    {
+        public koredb.Task GetTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<koredb.Task> GetTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
