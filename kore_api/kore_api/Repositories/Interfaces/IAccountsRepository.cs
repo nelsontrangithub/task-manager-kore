8f9d1ac1ba7dc66d30ca20527a5ef1887de591b6
nelsontrangithub <nelsontran@icloud.com>
2019-04-25 13:29:11 -0700

    added method to get all Accounts by User

--- kore_api/kore_api/Repositories/Interfaces/IAccountsRepository.cs
+++ kore_api/kore_api/Repositories/Interfaces/IAccountsRepository.cs
@@ -10,6 +10,7 @@ namespace kore_api.Repositories
     public interface IAccountsRepository
     {
         IEnumerable<AccountVM> GetAccounts();
+        IEnumerable<AccountVM> GetAccountsByUser(int userID);
         Task<Account> GetAccount(int id);
         Task<bool> Update(int id, int status);
         Task<bool> Delete(int id);
