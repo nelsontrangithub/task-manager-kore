0b251829adfba507b2f18c3e23e8aeab5145350c
Jonathan <joportoibarrondo@my.bcit.ca>
2019-04-26 11:12:30 -0700

    wip

--- kore_app/lib/models/task.dart
+++ kore_app/lib/models/task.dart
@@ -2,27 +2,38 @@ import 'package:flutter/material.dart';
 
 class Task {
   int id;
-  int ownerId;
-  String title;
-  bool isCompleted;
+  String subject;
+  int taskType;
+  int department;
   String description;
   DateTime dueDate;
+  DateTime  completedOn;
   DateTime dateCreated;
+  DateTime dateModified;
   int status;
+  int createdBy;
+  int modifiedBy;
+  bool isCompleted;
   Object color;
   Object icon;
   String label;
 
-  //Task(this.id, this.title, this.isCompleted, this.description, this.dueDate, this.color, this.icon, this.label);
+  Task({
+    this.id,
+    this.subject,
+    this.taskType,
+    this.department,
+    this.description,
+    this.dueDate,
+    this.completedOn,
+    this.dateCreated,
+    this.dateModified,
+    this.status,
+    this.createdBy,
+    this.modifiedBy,
+    }
 
-  Task(int id, String title, bool isCompleted, String description,
-      DateTime dueDate) {
-    this.id = id;
-    this.title = title;
-    this.isCompleted = isCompleted;
-    this.description = description;
-    this.dueDate = dueDate;
-
+  {
     if (isCompleted == true) {
       this.color = Colors.green[800];
       this.icon = Icons.done;
@@ -33,6 +44,9 @@ class Task {
       this.label = "Not Complete";
     }
   }
+
+  
+
 
   void setStatus(){
     if (isCompleted == true) {
